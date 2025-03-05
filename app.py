8866f431cc5d6b0ab8c5f7e41131fbc67a1763a0
Jorgegcd <jorgegocordi@gmail.com>
2025-03-05 05:36:50 +0100

    cambios en codigo pequeños

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ from check_password import check_password, logout
 def main():
     st.title("Tarea Módulo 8: Iniciar Sesión")
     # Leemos el parámetro 'page' de la URL; si no existe, asumimos "login"
-    params = st.experimental_get_query_params()
+    params = st.query_params()
     page = params.get("page", ["login"])[0]
 
     if page == "login":
 
         if check_password():
             # Una vez que se loguea, actualizamos el parámetro a "dashboard"
-            st.experimental_set_query_params(page="dashboard")
+            st.query_params(page="dashboard")
             st.experimental_rerun()
     elif page == "dashboard":
         if st.session_state.get('logged_in', False):        
@@ -21,7 +21,7 @@ def main():
                 logout()
         else:
             # Si no está logueado, volvemos al login
-            st.experimental_set_query_params(page="login")
+            st.query_params(page="login")
             st.experimental_rerun()
 
 if __name__ == "__main__":

51e9e3c5f77f774eeb8dd4dba9aa76d5b76b1853
Jorgegcd <jorgegocordi@gmail.com>
2025-03-05 05:04:15 +0100

    introducimos pulsador para login en app

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from auth import login_form, logout_button
+
+# Inicializamos el estado de sesión si aún no está definido
+if "logged_in" not in st.session_state:
+    st.session_state["logged_in"] = False
+
+if st.session_state["logged_in"]:
+    st.write("¡Bienvenido a la aplicación!")
+    # Aquí pones el contenido que quieres que vean los usuarios autenticados
+    st.write("Este es el contenido protegido de tu app.")
+    logout_button()
+else:
+    login_form()

9618f0c0813dced541c8f313fc81e16a49c63295
Jorgegcd <jorgegocordi@gmail.com>
2025-03-05 21:10:56 +0100

    intentamos generar botón de salir

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ def main():
             # Una vez que se loguea, actualizamos el parámetro a "dashboard"
             st.query_params = {'page':"dashboard"}
             st.rerun()
+            
     elif page == "dashboard":
         if st.session_state.get('logged_in', False):        
             st.success("Accediste sin problema")
@@ -23,11 +24,12 @@ def main():
             # Si no está logueado, volvemos al login
             st.query_params = {"page": "login"}
             st.rerun()
+
     btnSalir=st.button("Salir")
     if btnSalir:
         st.session_state.clear()
         # Luego de borrar el Session State reiniciamos la app para mostrar la opción de usuario y clave
         st.rerun()
-        
+
 if __name__ == "__main__":
     main()

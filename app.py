6ad1f2cfb517ce3a90d32376a0e087dc8d6507c4
Jorgegcd <jorgegocordi@gmail.com>
2025-03-05 21:22:06 +0100

    intentamos mostrar la página y que salga el botón de salir

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 from check_password import check_password, logout
 
 def main():
-    
     # Leemos el parámetro 'page' de la URL; si no existe, asumimos "login"
     params = st.query_params
     page = params.get("page", ["login"])[0]
@@ -11,19 +10,19 @@ def main():
         st.title("Tarea Módulo 8: Iniciar Sesión")
         if check_password():
             # Una vez que se loguea, actualizamos el parámetro a "dashboard"
-            st.query_params = {'page':"dashboard"}
+            st.set_query_params(page="dashboard")
             st.rerun()
 
     elif page == "dashboard":
         if st.session_state.get('logged_in', False):        
             st.success("Accediste sin problema")
             st.write("Bienvenido a la aplicación protegida.")
-            btnSalir=st.button("Cerrar Sesión")
+            btnSalir = st.button("Cerrar Sesión")
             if btnSalir:
                 logout()
         else:
             # Si no está logueado, volvemos al login
-            st.query_params = {"page": "login"}
+            st.set_query_params(page="login")
             st.rerun()
 
 if __name__ == "__main__":

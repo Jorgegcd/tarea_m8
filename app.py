21fbb7b37c3413eac249592c74e96a8afc268663
Jorgegcd <jorgegocordi@gmail.com>
2025-03-05 21:14:35 +0100

    intentamos mostrar la página

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from check_password import check_password, logout
 
 def main():
-    st.title("Tarea Módulo 8: Iniciar Sesión")
+    
     # Leemos el parámetro 'page' de la URL; si no existe, asumimos "login"
     params = st.query_params
     page = params.get("page", ["login"])[0]
@@ -11,9 +11,10 @@ def main():
 
         if check_password():
             # Una vez que se loguea, actualizamos el parámetro a "dashboard"
+            st.title("Tarea Módulo 8: Iniciar Sesión")
             st.query_params = {'page':"dashboard"}
             st.rerun()
-            
+
     elif page == "dashboard":
         if st.session_state.get('logged_in', False):        
             st.success("Accediste sin problema")

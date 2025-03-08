4f7dc1d97455461705665a063a76a62d8a8f4ec2
Jorgegcd <jorgegocordi@gmail.com>
2025-03-08 09:44:06 +0100

    se añaden botones en la página principal

--- home.py
+++ home.py
@@ -9,12 +9,17 @@ login.generarLogin()
 
 if 'usuario' in st.session_state:
     st.subheader('Información página principal')
+
+    col1, col2, col3 = st.columns(3)
     
-    if st.button("Ir a Stats"):
-        st.switch_page("Stats")  # Asegurate que el nombre "Stats" coincida con el título o nombre de la página en la carpeta pages.
-    
-    if st.button("Ir a Players"):
-        st.switch_page("Players") # Asegurate que el nombre "Players" coincida con el título o nombre de la página en la carpeta pages.
-    
-    if st.button("Ir a Teams"):
-        st.switch_page("Teams") # Asegurate que el nombre "Teams" coincida con el título o nombre de la página en la carpeta pages.
+    with col1:
+        if st.button("Ir a Stats"):
+            st.switch_page("Stats")  # Asegurate que el nombre "Stats" coincida con el título o nombre de la página en la carpeta pages.
+
+    with col2:    
+        if st.button("Ir a Players"):
+            st.switch_page("Players") # Asegurate que el nombre "Players" coincida con el título o nombre de la página en la carpeta pages.
+
+    with col3:    
+        if st.button("Ir a Teams"):
+            st.switch_page("Teams") # Asegurate que el nombre "Teams" coincida con el título o nombre de la página en la carpeta pages.

5a2f6d7e6ee553dc0e1b3ab8ccaad69961d902c9
Jorgegcd <jorgegocordi@gmail.com>
2025-03-08 10:46:19 +0100

    cambiamos nombres de las páginas

--- pages/pagina1.py
+++ pages/pagina1.py
@@ -1,2 +1,14 @@
 import streamlit as st
+import common.menu as menu
+
+# Configurar la página para que el botón de navegación vaya hasta el principio cuando se abre la página.
 st.set_page_config(page_title="Stats")
+
+# Mostrar el menú lateral si el usuario está logueado.
+if 'usuario' in st.session_state:
+    menu.generarMenu(st.session_state['usuario'])
+else:
+    st.write("Por favor, inicia sesión para ver el menú.")
+
+st.header("Estadísticas - Stats")
+st.write("Contenido de la página de estadísticas")

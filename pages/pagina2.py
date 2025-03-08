181cbdf96897eb02bebf2b87012b46326fa1c301
Jorgegcd <jorgegocordi@gmail.com>
2025-03-08 11:19:28 +0100

    cambiamos nombres de títulos de páginas

--- pages/pagina2.py
+++ pages/pagina2.py
@@ -2,7 +2,7 @@ import streamlit as st
 import common.menu as menu
 
 # Configurar la página para que el botón de navegación vaya hasta el principio cuando se abre la página.
-st.set_page_config(page_title="Stats") # Cambiamos nombre de la página
+st.set_page_config(page_title="Players") # Cambiamos nombre de la página
 
 # Mostrar el menú lateral si el usuario está logueado.
 if 'usuario' in st.session_state:

abf6a58d347c7f85cee36513bb7c2fa6d5284377
Jorgegcd <jorgegocordi@gmail.com>
2025-03-08 09:27:52 +0100

    cambio estados inicio sesión

--- common/login.py
+++ common/login.py
@@ -4,24 +4,24 @@ import os
 import common.menu as menu
 
 def generarLogin():
-    """Genera la ventana de login o muestra el menú si el login es valido
-    """    
-    # Validamos si el usuario ya fue ingresado    
-    if 'usuario' in st.session_state:
+    """Genera la ventana de login o muestra el menú si el login es valido"""    
+    # Si el usuario ya está logueado, mostramos el menú   
+    if st.session_state.get("logged_in", False) and 'usuario' in st.session_state:
         menu.generarMenu(st.session_state['usuario']) # Si ya hay usuario cargamos el menu        
     else: 
         # Cargamos el formulario de login       
         with st.form('frmLogin'):
             username = st.text_input('Usuario')
             password = st.text_input('Password',type='password')
-            btnLogin=st.form_submit_button('Entrar',type='primary')
-            if btnLogin:
+            boton_login=st.form_submit_button('Entrar',type='primary')
+            if boton_login:
                 if username == "admin" and password == "admin":
                     st.session_state['usuario'] = username
+                    st.session_state["logged_in"] = True
                     # Si el usuario es correcto reiniciamos la app para que se cargue el menú
                     st.rerun()
                 else:
-                    # Si el usuario es invalido, mostramos el mensaje de error
+                    # Si el usuario no es válido, mostramos el mensaje de error
                     st.error("Usuario o clave inválidos",icon=":material/gpp_maybe:")     
 
 def logout():

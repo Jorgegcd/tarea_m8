4c2d2f42663c4482be26d5f241cf440fe55aeea4
Jorgegcd <jorgegocordi@gmail.com>
2025-03-08 09:59:38 +0100

    cambiamos cosas de las páginas

---
+++ pages/pagina1.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import common.menu as menu
+
+# Verificamos si el usuario está logueado
+if 'usuario' in st.session_state:
+    menu.generarMenu(st.session_state['usuario'])
+else:
+    st.write("Por favor, inicia sesión para ver el menú.")
+
+st.header("Estadísticas Equipos")
+st.write("Contenido Estadísticas Equipos")

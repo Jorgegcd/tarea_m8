24d9d272447b25860bf81ba76a7035e2eaef753f
Jorgegcd <jorgegocordi@gmail.com>
2025-03-08 09:43:54 +0100

    se añaden botones en la página principal

--- home.py
+++ home.py
@@ -1,7 +1,20 @@
 import streamlit as st
 import common.login as login
 
+import streamlit as st
+import common.login as login
+
 st.header('Página principal')
 login.generarLogin()
+
 if 'usuario' in st.session_state:
     st.subheader('Información página principal')
+    
+    if st.button("Ir a Stats"):
+        st.switch_page("Stats")  # Asegurate que el nombre "Stats" coincida con el título o nombre de la página en la carpeta pages.
+    
+    if st.button("Ir a Players"):
+        st.switch_page("Players") # Asegurate que el nombre "Players" coincida con el título o nombre de la página en la carpeta pages.
+    
+    if st.button("Ir a Teams"):
+        st.switch_page("Teams") # Asegurate que el nombre "Teams" coincida con el título o nombre de la página en la carpeta pages.

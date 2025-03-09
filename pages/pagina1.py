c3b0970fcb74730cfa043c54c2077b8b9de53b71
Jorgegcd <jorgegocordi@gmail.com>
2025-03-09 13:17:42 +0100

    redondeamos a 2 decimales

--- pages/pagina1.py
+++ pages/pagina1.py
@@ -44,6 +44,8 @@ if "selected_season" in st.session_state:
     elif len(selected_teams) > 0:
         # Filtramos el dataframe para los equipos seleccionados
         df_filtrado = df_temporada[df_temporada['team_name'].isin(selected_teams)]
+        # Redondeamos los datos a 2 decimales
+        df_filtrado = df_filtrado.round(2)
         # Aplica un estilo básico para centrar el texto (esto es solo un ejemplo)
         styled_df = df_filtrado.style.set_properties(**{'text-align': 'center'})
         st.dataframe(styled_df)
